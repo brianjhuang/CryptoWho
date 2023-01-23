babf43c192c9554ed77bb0847fe0971bb5d7ed32
Brian Huang <brianjhuang25@gmail.com>
2023-01-23 13:04:06 -0800

    set up config folder

---
+++ config/YouTube.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import os
+
+class YouTube(object):
+    """
+    Static class that includes configuration for our YouTube Data Downloader
+    """
+
+    # General Config
+    DOWNLOAD_SNIPPET = True
+    DOWNLOAD_VIDEO_TAGS = True
+    DOWNLOAD_VIDEO_TRANSCRIPT = True
+    DOWNLOAD_VIDEO_COMMENTS = True
+
+
+    # YouTube Data API Config
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"
+    # Used by default when an API Key is not provided to the YouTubeVideoDownloader class
+    YOUTUBE_DATA_API_KEY = os.environ.get('YOUTUBE_DATA_API_KEY')
+
+    # Set the Number of Comments
+    LIMIT_PAGES_COMMENTS = 1  # 200 Comments per page
+
+    # # Recommended Videos
+    # RETRIEVE_RECOMMENDED_VIDEOS = False
+    # RECOMMENDED_VIDEOS_THRESHOLD = 10
