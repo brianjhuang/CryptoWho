5c105cb639c4d3f52d32545fb8a994769a9944bf
Brian Huang <brianjhuang25@gmail.com>
2023-01-23 13:06:18 -0800

    Added Reddit Config File

---
+++ config/Reddit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os
+
+class Reddit(object):
+    """
+    Static class that includes configuration for our Reddit Data Scraper
+    """
+
+    # General Config
+    SUBREDDITS = ['investing', 'finance', 'cryptocurrency', 'wallstreetbets'] 
--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,5 @@
 from .YouTube import YouTube
+from .Reddit import Reddit
 
 youtube = YouTube()
+reddit = Reddit()
