86790bd28a07292e5d9438c25e498ef7c12b8f41
brianjhuang <brianjhuang25@gmail.com>
2023-02-25 18:43:39 -0800

    updated webiste without using semantic ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
 
 function App() {
   return (
     <>
-    <div>Hello World</div>
+      <section>
+        <h1 style={{ fontSize: 50 }}>
+          CryptoWho
+          <strong
+            style={{
+              display: "block",
+              fontSize: "40%",
+              color: "#CCD1D1",
+              opacity: 0.75,
+            }}
+          >
+            Auditing YouTube Investment Recommendations using GPT-3
+          </strong>
+        </h1>
+      </section>
+
+      <section>
+        <h2>Abstract</h2>
+        <p>
+          {"Lorem Ipsum :)"}
+        </p>
+      </section>
+
+      <section>
+        <h2>Methodology</h2>
+        <p>
+          {"Lorem Ipsum :)"}
+        </p>
+      </section>
     </>
   );
 }
