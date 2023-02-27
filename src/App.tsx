9ab7e49c626be1efbd3965de2f6c3157d2128338
brianjhuang <brianjhuang25@gmail.com>
2023-02-27 02:21:47 -0800

    Proper padding

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,10 @@ function App() {
           backgroundImage: `url(${process.env.PUBLIC_URL}/images/cryptowho.png)`,
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          paddingTop: "100px"
         }}
       >
-        <h1 style={{ fontSize: 50 }}>
+        <h1 style={{ fontSize: 50, paddingTop: "200px"}}>
           CryptoWho
           <strong
             style={{
