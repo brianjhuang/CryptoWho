f11546ce1831d2487231570814fcdea27ee72b57
Lily Yu <alyu@ucsd.edu>
2023-03-01 11:53:44 -0800

    Create audit config file

---
+++ config/Audit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import os
+
+
+class Audit(object):
+    """
+    Static class that includes configuration for our Audit process
+    """
+
+    #AUDIT VARIABLES
+    USER_AGE = 'young' #'young' or 'old'
+    FINANCE_VIDEO_TYPE = 'traditional' #'traditional', 'blockchain', 'mixed', 'unrelated'
+
+    WATCH_DURATION = 10 #Number of seconds to watch each video
+
+    #PATH VARIABLES
+    SEED_AGE_VIDEO_PATH = 'data/raw/youtube/seed_age_videos.csv'
+    SEED_FINANCE_VIDEO_PATH = 'data/raw/youtube/seed_finance_videos.csv'
+
+    AUDIT_RESULTS_PATH = 'data/audit/youtube/'
